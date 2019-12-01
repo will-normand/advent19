84ca84ba5e7e0ce8884f230218d0100e45da687d
Will Normand <mail@wnormand.co.uk>
2019-12-01 19:28:44 +0000

    Tidy up day 1

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,39 @@
-use std::fs;
-
 fn main() {
-    let result = day1();
+    let result = day1::day1();
     println!("Day 1 result is: {}", result)
 }
 
-fn day1() -> i32 {
-    println!("Running day 1");
-    let masses = load_file("src/day1.txt".to_string());
-    
-    masses.iter().map(|mass| compound_fuel(mass)).sum()
-}
+mod day1 {
+    use std::fs;
 
-fn load_file(filename: String) -> Vec<i32> {
-    let masses = fs::read_to_string(filename).expect("Failed to find file");
-    masses.split('\n').map(|s| s.parse::<i32>().unwrap()).collect() 
-}
+    pub fn day1() -> i32 {
+        println!("Running day 1");
+        let masses = load_file("data/day1.txt".to_string());
 
-fn calculate_fuel(mass: &i32) -> i32 {
-    (mass / 3) - 2
-}
-
-fn compound_fuel(mass: &i32) -> i32 {
-    let mut fuel = calculate_fuel(mass);
-    let mut extra_fuel = fuel;
-
-    while extra_fuel > 0 {
-        extra_fuel = calculate_fuel(&extra_fuel);
-        fuel += if extra_fuel > 0 { extra_fuel } else { 0 }
+        masses.iter().map(|mass| compound_fuel(*mass)).sum()
     }
 
-    return fuel
+    fn load_file(filename: String) -> Vec<i32> {
+        let masses = fs::read_to_string(filename).expect("Failed to find file");
+        masses
+            .split('\n')
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect()
+    }
+
+    fn calculate_fuel(mass: i32) -> i32 {
+        (mass / 3) - 2
+    }
+
+    fn compound_fuel(mass: i32) -> i32 {
+        let mut fuel = calculate_fuel(mass);
+        let mut extra_fuel = fuel;
+
+        while extra_fuel > 0 {
+            extra_fuel = calculate_fuel(extra_fuel);
+            fuel += if extra_fuel > 0 { extra_fuel } else { 0 }
+        }
+
+        fuel
+    }
 }
