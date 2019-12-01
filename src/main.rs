7dba68b75f9c8d076826f04c246b70dfb1865aac
Will Normand <mail@wnormand.co.uk>
2019-12-01 19:03:47 +0000

    AoC: day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let result = day1();
+    println!("Day 1 result is: {}", result)
+}
+
+fn day1() -> i32 {
+    println!("Running day 1");
+    let masses = load_file("src/day1.txt".to_string());
+    
+    masses.iter().map(|mass| compound_fuel(mass)).sum()
+}
+
+fn load_file(filename: String) -> Vec<i32> {
+    let masses = fs::read_to_string(filename).expect("Failed to find file");
+    masses.split('\n').map(|s| s.parse::<i32>().unwrap()).collect() 
+}
+
+fn calculate_fuel(mass: &i32) -> i32 {
+    (mass / 3) - 2
+}
+
+fn compound_fuel(mass: &i32) -> i32 {
+    let mut fuel = calculate_fuel(mass);
+    let mut extra_fuel = fuel;
+
+    while extra_fuel > 0 {
+        extra_fuel = calculate_fuel(&extra_fuel);
+        fuel += if extra_fuel > 0 { extra_fuel } else { 0 }
+    }
+
+    return fuel
 }
