27f797e522f59f2230a6b774cd6f161569ace8f2
Will Normand <will.normand@fanduel.com>
2019-12-03 13:50:10 +0000

    Instruction enum

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,12 @@ mod day2 {
 
     const TARGET: i32 = 19_690_720;
 
+    enum Instruction {
+        ADD(usize, usize, usize),
+        MUL { a: usize, b: usize, result: usize },
+        TERM,
+    }
+
     pub fn run() {
         let input = load_file("data/day2.txt".to_string());
 
@@ -77,23 +83,43 @@ mod day2 {
             .collect()
     }
 
+    fn get_instruction(memory: &[i32], instruction_pointer: usize) -> Instruction {
+        let opcode = memory[instruction_pointer];
+
+        if opcode == 99 {
+            Instruction::TERM
+        } else if opcode == 1 {
+            Instruction::ADD(
+                memory[instruction_pointer + 1] as usize,
+                memory[instruction_pointer + 2] as usize,
+                memory[instruction_pointer + 3] as usize,
+            )
+        } else if opcode == 2 {
+            Instruction::MUL {
+                a: memory[instruction_pointer + 1] as usize,
+                b: memory[instruction_pointer + 2] as usize,
+                result: memory[instruction_pointer + 3] as usize,
+            }
+        } else {
+            panic!();
+        }
+    }
+
     fn process(mut memory: Vec<i32>) -> Vec<i32> {
         let mut i = 0;
         loop {
-            let opcode = memory[i];
+            let instruction: Instruction = get_instruction(&memory, i);
 
-            if opcode == 99 {
-                break;
-            }
-
-            let a: usize = memory[i + 1] as usize;
-            let b: usize = memory[i + 2] as usize;
-            let c: usize = memory[i + 3] as usize;
-
-            if opcode == 1 {
-                memory[c] = memory[a] + memory[b];
-            } else if opcode == 2 {
-                memory[c] = memory[a] * memory[b];
+            match instruction {
+                Instruction::ADD(a, b, result) => {
+                    memory[result] = memory[a] + memory[b];
+                }
+                Instruction::MUL { a, b, result } => {
+                    memory[result] = memory[a] * memory[b];
+                }
+                Instruction::TERM => {
+                    break;
+                }
             }
 
             i += 4;
